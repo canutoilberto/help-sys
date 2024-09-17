18c7d3931762ed2f101ea940c063daf69daaf8af
canutoilberto <canutoilberto@gmail.com>
2024-09-17 12:29:00 -0300

    [Refact] Home compoent refactored

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,36 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useUserStore } from "@/store/userStore";
 
 export default function Home() {
   const user = useUserStore((state) => state.user);
   const router = useRouter();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    if (user) {
-      router.push("/dashboard");
-    } else {
-      router.push("/auth/login");
-    }
+    const redirect = () => {
+      try {
+        if (user) {
+          router.push("/dashboard");
+        } else {
+          router.push("/auth/login");
+        }
+      } catch (error) {
+        console.error("Erro ao redirecionar", error);
+        // Você pode adicionar uma lógica de tratamento de erro aqui
+      } finally {
+        setLoading(false); // Define como carregamento concluído
+      }
+    };
+
+    redirect();
   }, [user, router]);
 
-  return <div>Redirecionando...</div>;
+  if (loading) {
+    return <div>Redirecionando...</div>; // Feedback de carregamento
+  }
+
+  return null; // Não retorna nada após o redirecionamento
 }
