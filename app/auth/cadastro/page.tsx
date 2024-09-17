c3ff59dcafe1f381e0ea6f494ecee6ab4ac582aa
canutoilberto <canutoilberto@gmail.com>
2024-09-17 11:41:30 -0300

    [Refact] FormField implemented in this component

--- app/auth/cadastro/page.tsx
+++ app/auth/cadastro/page.tsx
@@ -2,12 +2,11 @@
 
 import { useState } from "react";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-import { auth } from "@/lib/firebase";
+import { auth, FirebaseError } from "@/lib/firebase";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { FormField } from "@/components/FormField";
 import {
   Card,
   CardContent,
@@ -28,8 +27,13 @@ export default function CadastroPage() {
 
   const handleCadastro = async (e: React.FormEvent) => {
     e.preventDefault();
+    setError("");
     if (senha !== confirmarSenha) {
       setError("As senhas não coincidem.");
+      return;
+    }
+    if (senha.length < 6) {
+      setError("A senha deve conter pelo menos 6 caracteres.");
       return;
     }
     try {
@@ -43,7 +47,27 @@ export default function CadastroPage() {
       setUser(userCredential.user);
       router.push("/auth/login");
     } catch (error) {
-      setError("Falha no cadastro. Por favor, tente novamente.");
+      if (error instanceof FirebaseError) {
+        handleFirebaseErrors(error);
+      } else {
+        setError("Falha no cadastro. Por favor, tente novamente.");
+      }
+    }
+  };
+
+  const handleFirebaseErrors = (error: FirebaseError) => {
+    switch (error.code) {
+      case "auth/email-alreadi-in-use":
+        setError("Este email já está em uso.");
+        break;
+      case "auth/invalid-email":
+        setError("Formato de e-mail inválido.");
+        break;
+      case "auth/weak-password":
+        setError("A senha é muito fraca.");
+        break;
+      default:
+        setError("Erro desconhecido. Por favor, tente novamente.");
     }
   };
 
@@ -57,50 +81,42 @@ export default function CadastroPage() {
         </CardHeader>
         <CardContent>
           <form onSubmit={handleCadastro} className="space-y-4">
-            <div className="space-y-4">
-              <div className="space-y-2">
-                <Label htmlFor="nome">Nome</Label>
-                <Input
-                  id="nome"
-                  type="text"
-                  placeholder="Seu nome completo"
-                  required
-                  value={nome}
-                  onChange={(e) => setNome(e.target.value)}
-                />
-              </div>
-              <div className="space-y-2">
-                <Label htmlFor="email">E-mail</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="seu@email.com"
-                  required
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
-              <div className="space-y-2">
-                <Label htmlFor="senha">Senha</Label>
-                <Input
-                  id="senha"
-                  type="password"
-                  required
-                  value={senha}
-                  onChange={(e) => setSenha(e.target.value)}
-                />
-              </div>
-              <div className="space-y-2">
-                <Label htmlFor="confirmarSenha">Confirmar Senha</Label>
-                <Input
-                  id="confirmarSenha"
-                  type="password"
-                  required
-                  value={confirmarSenha}
-                  onChange={(e) => setConfirmarSenha(e.target.value)}
-                />
-              </div>
-            </div>
+            <FormField
+              id="nome"
+              label="Nome"
+              type="text"
+              placeholder="Seu nome completo"
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+              required
+            />
+            <FormField
+              id="email"
+              label="E-mail"
+              type="email"
+              placeholder="seu@email.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <FormField
+              id="senha"
+              label="Senha"
+              type="password"
+              placeholder="Digite sua senha"
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
+              required
+            />
+            <FormField
+              id="confirmarSenha"
+              label="Confirmar Senha"
+              type="password"
+              placeholder="Confirme sua senha"
+              value={confirmarSenha}
+              onChange={(e) => setConfirmarSenha(e.target.value)}
+              required
+            />
             {error && <p className="text-red-500 text-sm mt-2">{error}</p>}
             <Button className="w-full mt-4" type="submit">
               Cadastrar
