c7c8416de0fc622c450018a48174b68bfaac0d07
canutoilberto <canutoilberto@gmail.com>
2024-09-17 14:37:31 -0300

    [Refact] Routes refacted

--- components/ProtectedLayout.tsx
+++ components/ProtectedLayout.tsx
@@ -2,22 +2,25 @@
 
 import { useUserStore } from "@/store/userStore";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const ProtectedLayout: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const user = useUserStore((state) => state.user);
   const router = useRouter();
+  const [isRedirecting, setIsRedirecting] = useState(true);
 
   useEffect(() => {
-    if (!user) {
+    if (user === null) {
       router.push("/auth/login");
+    } else {
+      setIsRedirecting(false);
     }
   }, [user, router]);
 
-  if (!user) {
-    return null;
+  if (isRedirecting) {
+    return <div>Loading...</div>; // Indicador de carregamento
   }
 
   return <>{children}</>;
